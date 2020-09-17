55b05a763890418250f3991d5c0123a773ec8b1c
Nguyen The Thang Long <longntt98vn@gmail.com>
2020-09-17 18:41:54 +0700

    abc

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
+import firebase from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/database";
 
 //this config is being used for both development and production environment. Though, it is a best practice creating a second project and have two configs: one for production (prodConfig) and another for development (devConfig), so you choose the config based on the environment.
 
